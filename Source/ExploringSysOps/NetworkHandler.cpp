6be8f0d22a94758bd08a51d3392154d5a73f1556
SimpleCookie <nanashi1337@live.se>
2016-09-14 13:17:40 +0200

    Initiate the project

---
+++ Source/ExploringSysOps/NetworkHandler.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ExploringSysOps.h"
+#include "NetworkHandler.h"
+
+NetworkHandler::NetworkHandler()
+{
+}
+
+NetworkHandler::~NetworkHandler()
+{
+}
