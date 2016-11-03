bdce25a34e89399d064ef04b96f8ea06e54986bc
SimpleCookie <nanashi1337@live.se>
2016-11-03 13:41:36 +0100

    Attempt to add instantiation functionality of plugins
    Add a instantiate function in the plugin-manager and update the PluginObject with proper package.dir and slug. Also do additional modifications to the plugin-manager

--- Content/Scripts/main.js
+++ Content/Scripts/main.js
@@ -9,24 +9,29 @@
  **/
 
 
-"use strict"
+// "use strict"
 
 
 // This function is called when the javascript UE library is loaded
 function main() {
+	var pluginManager = require('plugin-manager');
 	(function testcode_for_plugins() {
-		var plugins = require('plugin-manager');
-
-		plugins.TogglePluginList(); // Show/hide pluginlist
-		plugins.List().then(function(list) {
-			list.forEach(function(plugin) {
-				plugin.Install().then(function() {
-					// This is for updating the list
-					plugins.TogglePluginList();
-					plugins.TogglePluginList();
-				});
-			});
-		});
+		var location = {X: -230, Y: -230, Z:450};
+		var rotation = {ROLL: 0, PITCH: 0, YAW: 0};
+		// var plugin1 = pluginManager.Instantiate('simsdewire_sims-plugins', 0, location, rotation);
+		// setTimeout(function() {
+		// 	console.log(pluginManager.Destroy(plugin1));
+		// }, 3000);
+		// plugins.TogglePluginList(); // Show/hide pluginlist
+		// plugins.List().then(function(list) {
+		// 	list.forEach(function(plugin) {
+		// 		plugin.Install().then(function() {
+		// 			// This is for updating the list
+		// 			plugins.TogglePluginList();
+		// 			plugins.TogglePluginList();
+		// 		});
+		// 	});
+		// });
 	})();
 	
 	/**
@@ -35,12 +40,60 @@ function main() {
 	const uclass = require('uclass')().bind(this,global)
 	class ScriptHandler_S extends Blueprint.Load('/Game/BlueprintScripts/ScriptManager').GeneratedClass {
 		ReceiveBeginPlay() {
+			super.ReceiveBeginPlay();
+			console.log("hej");
+		}
+		Print(params) {
+			console.log("innetoa", params);
+		}
+		InstantiatePluginActorOverride(pluginName, pluginActor, location, rotation) {
+			var returnValue = false !== pluginManager.Instantiate(pluginName, pluginActor, location, rotation);
+			
+			// First 4 parameters are required but has no meaning for the super-function..
+			// The last paramter is a boolean, returns true if success, false if failed
+			return super.InstantiatePluginActorOverride("", "", {}, {}, returnValue);
+		}
+		DestroyPluginActorOverride(actorPlugin) {
+			var returnValue = false !== pluginManager.Destroy(actorPlugin);
+
+			// First parameter are required but has no meaning for the super-function..
+			// The last paramter is a boolean, returns true if success, false if failed
+			return super.DestroyPluginActorOverride({}, returnValue);
+		}
+		GetAvailablePluginListOverride() {
+			return super.GetAvailablePluginListOverride(pluginManager.getAvailablePluginList().map(function(plugin) {
+				return plugin.packageSlug;
+			}));
+		}
+		GetInstalledPluginListOverride() {
+			var actors = pluginManager.getInstalledPluginList();
+			var actorNames = [];
+			for(var i in actors)
+				actorNames.push(i);
+			return super.GetInstalledPluginListOverride(actorNames);
+		}
+		GetPluginActorsOverride(slug) {
+			var actors = pluginManager.GetPluginActors(slug);
+			var actorNames = [];
+			for(var i in actors)
+				actorNames.push(i);
+			return super.GetPluginActorsOverride("", actorNames);
+		}
+		GetPluginActorImageOverride(slug) {
+			throw new Error("Not implemented: GetPluginActorImageOverride(" + slug + ")");
+		}
+		GetPluginActorDescriptionOverride(slug, actorName) {
+			var actors = pluginManager.GetPluginActors(slug);
+			if(actors != false && actorName in actors)
+				return super.GetPluginActorDescriptionOverride("", "", actors[actorName].description);
+			return super.GetPluginActorDescriptionOverride("", "", "");
 		}
 	}      
 	var ScriptHandler = uclass(ScriptHandler_S);
-	
+	var sh_instance = new ScriptHandler(GWorld);
 	// clean up the mess
 	return function () {
+		sh_instance.DestroyActor();
 	}
 }
 
